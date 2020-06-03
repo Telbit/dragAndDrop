3b207c638ffc5849869c9fb5f348358e52d53f31
Telbit <bendeguz.telegdi@gmail.com>
2020-06-04 00:17:45 +0200

    comments for js, few changes

--- static/js/main.js
+++ static/js/main.js
@@ -15,9 +15,12 @@ function init() {
         nodes[i].addEventListener("dragstart", (event) => {
             event.dataTransfer.setData('text', event.target.id);
 
+            // get all elements vertical position
             establishNodePosition()
 
             selectedNode = document.getElementById(event.target.id);
+
+            // get selected node's place in the list
             startPosition = getStartPosition();
 
             setTimeout(() => {
@@ -26,32 +29,39 @@ function init() {
         });
 
         nodes[i].addEventListener("dragend", (event) =>{
+
+            // drop the selected node to the start position if the drag ended without drop
             if (selectedNode !== dropzone.children[selectedNodePos]) {
                 dropzone.insertBefore(selectedNode, dropzone.children[startPosition]);
             }
+
             resetNodes();
+
             setTimeout(() => {
                 selectedNode.style.backgroundColor = 'cornsilk';
-                selectedNode.style.transition = '2s';
+                selectedNode.style.transition = '0.5s';
             }, 200);
         });
     }
 
     dropzone.addEventListener("dragover", (event) => {
         event.preventDefault();
+
+        // get the selected position's place in the nodes list
         whereAmI(event.clientY);
     });
 
     dropzone.addEventListener("drop", (event) => {
         event.preventDefault();
 
+        // insert the node to the selected position
         dropzone.insertBefore(selectedNode, dropzone.children[selectedNodePos]);
 
         resetNodes();
 
         setTimeout(() => {
             selectedNode.style.backgroundColor = 'cornsilk';
-            selectedNode.style.transition = '2s';
+            selectedNode.style.transition = '0.5s';
         }, 200);
     });
 }
@@ -74,8 +84,11 @@ function resetNodes() {
 }
 
 function whereAmI(currentYPos) {
+
+    // get all elements vertical position
     establishNodePosition()
 
+    // get the upper and the lower node
     for (let i = 0; i < nodes.length; i++) {
         if (nodes[i]['yPos'] < currentYPos) {
             var nodeAbove = document.getElementById(nodes[i]['id']);
@@ -90,11 +103,13 @@ function whereAmI(currentYPos) {
         selectedNodePos = 0;
     }
 
+    // console.log(selectedNodePos);
+
     resetNodes();
 
     if (typeof nodeBelow == 'object') {
         nodeBelow.style.marginTop = '3em';
-        nodeBelow.style.transition = '1.8s';
+        nodeBelow.style.transition = '0.5s';
     }
 }
 
